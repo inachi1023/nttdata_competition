130a417c9bf6ad2faf9abc952682dbac6b7bfbfd
junichiyamanaka <juntodoroki@gmail.com>
2020-05-31 13:15:55 +0900

    衛星写真(未処理)とGDPでプロットしてみた

--- test2_numpy_read_yamanaka.py
+++ test2_numpy_read_yamanaka.py
@@ -3,74 +3,69 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
+import pandas as pd
 
 
-def plot_img(image1, image2):
+def read_light_diff(image_before, image_after, light_diff):
+    """夜間光差分取得メソッド"""
+
     # 画像読み込む(グレースケール(白黒写真)として読み込み)
-    img1 = np.array(Image.open(image1).convert('L'))  # 変化前の画像
-    img2 = np.array(Image.open(image2).convert('L'))  # 変化後の画像(ライトが消えた)
-
-    # plt.imshow(img1)
-    # plt.imshow(img2)
-
-    # plt.show()
+    gray_before = np.array(Image.open(image_before).convert('L'))  # 変化前の画像
+    gray_after = np.array(Image.open(image_after).convert('L'))  # 変化後の画像
 
     # 配列に格納された値の総和を算出
-    arr_sum1 = img1.sum()  # 変化前
-    arr_sum2 = img2.sum()  # 変化後
-
-    # 配列を表示してみる
-    # np.set_printoptions(threshold=np.inf) # 配列を省略せず表示するにはコメント外す。
-    # print(arr_sum1)
-    # print(arr_sum2)
-
-    # ndarrayの形状（shape）をタプルで表したもの
-    print(img1.shape)
-    print(img2.shape)
-
-    # 総和を表示
-    print(arr_sum1)
-    print(arr_sum2)
+    # (デフォルト(uint64)では演算が正しくできないため、dtype指定)
+    sum_before = gray_before.sum(dtype=int)  # 変化前
+    sum_after = gray_after.sum(dtype=int)  # 変化後
 
     # 差分を計算
-    arr_difference = arr_sum1 - arr_sum2
+    difference_sum_gray = sum_after - sum_before
 
-    # 値がデカすぎるので調整
-    arr_difference_param = arr_difference / 1000000000
-    print(arr_difference)
-
-    # ファイル出力保存
-    img_gray1 = Image.fromarray(img1)
-    img_gray1.save('sample1_gray.jpg')
-    img_gray2 = Image.fromarray(img2)
-    img_gray2.save('sample2_gray.jpg')
-
-    # プロットしてみる
-    plt.title("economy to light")
-    plt.xlabel("light of difference")
-    plt.ylabel("Economic impact ")
-
-    x = [arr_difference_param]  # 夜間照明の差
-    y = [1.2]  # 経済影響(GDPとか)　※今回はテキトーに1.2%下がったとか
-
-    # x = np.random.rand(100)
-    # y = np.random.rand(100)
-
-    plt.scatter(x, y)
-
-    plt.savefig("plot_result.png")
-    # plt.show()
-
-    '''いろんな夜間照明画像突っ込んでみてプロットしてみると相関関係見えるかーと思った'''
+    # 夜間照明の差を設定
+    light_diff.append(difference_sum_gray)
 
 
-# 写真を設定
-files = sorted(glob.glob('night_image/*'))
+def read_economy_diff(value1, value2, economy_diff):
+    """経済指標差分取得メソッド"""
 
-for i in range(len(files) - 1):
-    night_img1 = files[i]
-    night_img2 = files[i + 1]
+    # 差分を計算
+    val_difference_param = value1 - value2
 
-    plot_img(night_img1, night_img2)
+    # 経済指標の差を設定
+    economy_diff.append(val_difference_param)
 
+
+"""メインメソッド"""
+
+x = []  # プロット_x軸
+y = []  # プロット_y軸
+
+years = range(1992, 2013 - 1)  # 1992年から2013年まで
+
+for year in years:
+    before_year = str(year)
+    after_year = str(year + 1)
+
+    # 写真(夜間光)を設定
+    night_img_before = 'night_image/' + before_year + '.jpg'
+    night_img_after = 'night_image/' + after_year + '.jpg'
+
+    # 夜間光差分取得
+    read_light_diff(night_img_before, night_img_after, x)
+
+    # CSV(経済指標)を設定
+    economy_index_before = pd.read_csv('economy_index/GDP.csv')[before_year][0]
+    economy_index_after = pd.read_csv('economy_index/GDP.csv')[after_year][0]
+
+    # 経済指標差分取得
+    read_economy_diff(economy_index_before, economy_index_after, y)
+
+
+# タイトル、軸名設定
+plt.title("economy to light")
+plt.xlabel("light of difference")
+plt.ylabel("Economic impact ")
+
+# 値をプロットする
+plt.scatter(x, y)
+plt.savefig("plot_result.png")
